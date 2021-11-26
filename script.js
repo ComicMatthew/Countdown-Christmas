3b844861689d77669e297a7e9d294907c2c1b5f3
ComicMatthew <mateusz.a.staniek@gmail.com>
2021-11-26 12:53:41 +0100

    final countdown

--- script.js
+++ script.js
@@ -2,3 +2,29 @@
 //testing if changes apply to GitHub
 
 //test test test 
+
+const days = document.getElementById('days')
+const hours = document.getElementById('hours')
+const minutes = document.getElementById('minutes')
+const secondss = document.getElementById('seconds')
+
+const christmas = "24 Dec 2021" ; 
+
+
+function countdown() { 
+    const christmasDate = new Date(christmas);
+    const currentDate = new Date()
+
+
+    const seconds= Math.floor((christmasDate - currentDate) / 1000);
+    
+    days.innerHTML = Math.floor(seconds / 3600 / 24)
+    hours.innerHTML = Math.floor((seconds / 3600) % 24)
+    minutes.innerHTML = Math.floor((seconds / 60) % 60)
+    secondss.innerHTML = seconds % 60;
+
+    
+}
+
+
+setInterval(countdown, 1000)
